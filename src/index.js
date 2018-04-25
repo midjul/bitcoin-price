3cbe2809e4de61d14ca71b9be3cf46f4d3a1a4ad
Midhet <djulovic_m@hotmail.com>
2018-04-25 17:29:09 +0200

    Add eslint suport for React and App comonent

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import styles from './index.css'
-
-const Index = () => {
-  return <div className={styles.body}>Hello React!</div>
-}
-
-ReactDOM.render(<Index />, document.getElementById('root'))
