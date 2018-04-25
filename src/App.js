027c8abc7cc6a03d7c62b0e596cd27620d4e5f15
Midhet <djulovic_m@hotmail.com>
2018-04-25 18:28:45 +0200

    Add jsx, jsconfig and typings for react and jsx

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react'
-
+import { Observable } from 'rxjs/Rx'
 class App extends Component {
+  componentDidMount () {
+    const url = symbol =>
+      `https://api.coindesk.com/v1/bpi/currentprice/${symbol}.json`
+    //    const interval$ = Observable.interval(500)
+    const symbols$ = Observable.of('bam', 'usd', 'eur')
+
+    symbols$
+      .map(url)
+      .mergeMap(url => Observable.ajax(url))
+      .map(res => res.response)
+      .subscribe(console.log)
+  }
   render () {
     return <div>Hello React!</div>
   }
